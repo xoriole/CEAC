bc2b8823edbc27ae0b0b41330608cb5bf9201212
Sandip Pandey <np.sandip.pandey@gmail.com>
2014-11-13 15:44:42 +0100

    First commit to upload to github

---
+++ src/com/sdm/phr/PHR.java
@@ -0,0 +1,54 @@
+package com.sdm.phr;
+
+import com.sdm.phr.cpabe.Cpabe;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author phoenix
+ */
+public class PHR {
+
+    static String doctor1_attr = "type:doctor hospital:A name:Doctor1";
+    static String nurse1_attr = "type:nurse hospital:A name:Nurse1";
+
+    static String doctor_policy = "type:doctor";
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, NoSuchAlgorithmException, Exception {
+        //System.loadLibrary("libpbc");
+        //System.loadLibrary("libjpbc-pbc");
+
+        String pubfile = "keys/public_key";
+        String mskfile = "keys/master_key";
+        String prvfile = "keys/private_key";
+
+        String inputfile = "data/input.pdf";
+        String encfile = "data/input.pdf.enc";
+        String decfile = "data/output.pdf";
+
+        String user_attr = nurse1_attr;
+//        String user_attr = doctor1_attr;
+        String policy = doctor_policy;
+
+        Cpabe test = new Cpabe();
+        test.setup(pubfile, mskfile);
+        System.out.println("//Setup complete (public key, master key)");
+
+//        test.keygen(pubfile, prvfile, mskfile, doctor1_attr);
+        test.keygen(pubfile, prvfile, mskfile, user_attr);
+        System.out.println("//Private key for " + user_attr);
+
+        //test.enc(pubfile, policy, inputfile, encfile);
+        System.out.println("//encrypting with policy:" + policy);
+
+        System.out.println("//Decrypting");
+        try {
+            test.dec(pubfile, prvfile, encfile, decfile);
+//        System.out.println("//end to dec");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/com/sdm/phr/bswabe/BswabePrv.java
@@ -0,0 +1,13 @@
+package com.sdm.phr.bswabe;
+
+import java.util.ArrayList;
+
+import it.unisa.dia.gas.jpbc.Element;
+
+public class BswabePrv {
+	/*
+	 * A private key
+	 */
+	Element d; /* G_2 */
+	ArrayList<BswabePrvComp> comps; /* BswabePrvComp */
+}
