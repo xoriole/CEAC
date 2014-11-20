c074a11535963c2b30b17eb9417d9297687e3c38
Herson Esquivel Vargas <herson.ev@gmail.com>
2014-11-20 08:11:24 -0600

    Additional tables for the token server

--- phr.sql
+++ phr.sql
@@ -94,3 +94,22 @@ INSERT INTO `user` (`uid`, `full_name`, `orgn_id`, `attributes`, `secret_key_che
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+/* Addtional table used by the token server */;
+CREATE TABLE IF NOT EXISTS `token_session` (
+  `requester_id` int(11) NOT NULL,
+  `p_id` int(11) NOT NULL,
+  `token` char(40) NOT NULL,
+  `created_on` timestamp DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 ;
+
+CREATE TABLE IF NOT EXISTS `health_data` (
+  `sid` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` int(11) NOT NULL,
+  `author_id` int(11) NOT NULL,
+  `aes_key` text NOT NULL,
+  `cipher_text` text NOT NULL,
+  `access_policy` text NOT NULL,
+  PRIMARY KEY (`sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
